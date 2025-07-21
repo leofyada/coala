0a0bb9d5d12810d2fe38b167e9f6140d8232fd78
Leonardo Fujisima Yada <leofyada@gmail.com>
2025-07-21 18:26:40 -0300

    Adjust files

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -8,3 +8,9 @@ source("code/functions.R")
 clean_data <- clean_raw_data(country_dfs)
 # Exporting clean data
 data.table::fwrite(x = clean_data, file = here("clean_data", "bid_data.csv"))
+
+#--------------------
+#- DATA PREPARATION - 
+#--------------------
+
+
--- code/data_import.R
+++ code/data_import.R
@@ -6,3 +6,5 @@
 source("code/functions.R")
 # Importing BID files per country
 country_dfs <- import_raw_data()
+# Importing product 1 excel file
+df_product1 <- readxl::read_xlsx(here("raw_data", "20250611_base_final.xlsx"))

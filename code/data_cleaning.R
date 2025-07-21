3fad2f81fa929406d7a2f3942df59425d8754f0f
Leonardo Fujisima Yada <leofyada@gmail.com>
2025-07-21 20:35:46 -0300

    Data cleaning commit

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -8,9 +8,5 @@ source("code/functions.R")
 clean_data <- clean_raw_data(country_dfs)
 # Exporting clean data
 data.table::fwrite(x = clean_data, file = here("clean_data", "bid_data.csv"))
-
-#--------------------
-#- DATA PREPARATION - 
-#--------------------
-
+writexl::write_xlsx(x = clean_data, path = here("clean_data", "bid_data.xlsx"))
 

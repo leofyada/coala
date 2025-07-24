4763bc38e40c05d7f3b0444dd7e28495ed883d39
Leonardo Fujisima Yada <leofyada@gmail.com>
2025-07-24 09:17:57 -0300

    Include final script

---
+++ code/data_final.R
@@ -0,0 +1,12 @@
+#--------------------------
+#- FINAL DATA PREPARATION - 
+#--------------------------
+
+# Importing functions
+source("code/functions.R")
+# Preparing final data
+final_data <- prepare_final_data(df_bid = clean_data_formatted, df_p1 = df_product1)
+
+# Exporting final data
+data.table::fwrite(x = final_data, file = here("clean_data", "final_data.csv"))
+writexl::write_xlsx(x = final_data, path = here("clean_data", "final_data.xlsx"))
